86e32b9563a3e2352e909cd031cd9dd127275cfc
unknown <thabhi098@gmail.com>
2025-02-10 15:45:07 +0530

    authentication updateed

--- Frontend/src/services/authentication.js
+++ Frontend/src/services/authentication.js
@@ -1,21 +1,6 @@
 import axios from "axios";
 
-export async function signin({ address }) {
-  try {
-    const res = await axios.post(`${BASE_URL}/${role}/${logOrSign}`, {
-      address,
-    });
-    console.log(res.data);
-    return { message: res.data.message, ...res.data };
-  } catch (err) {
-    console.log(err.response.data.message);
-    return {
-      status: err.response.data.status,
-      message: err.response.data.message,
-    };
-  }
-}
-export async function signup({ address }) {
+export async function signInUp({ address }) {
   try {
     const res = await axios.post(`${BASE_URL}/${role}/${logOrSign}`, {
       address,
